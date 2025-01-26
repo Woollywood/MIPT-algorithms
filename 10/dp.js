6e3f9f8f67d176fac9a45cdbd107e16260a7cc7e
Woollywood <115595112+Woollywood@users.noreply.github.com>
2025-01-26 06:00:51 +0300

    лекция 10 - дополнение

--- 10/dp.js
+++ 10/dp.js
@@ -1,11 +1,28 @@
 function fib(n) {
-	return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+	return n <= 1 ? n : fib(n - 2) + fib(n - 1);
+}
+
+const cache = new Map();
+
+function fibWithCache(n) {
+	if (n <= 1) {
+		return n;
+	}
+
+	if (cache.has(n)) {
+		return cache.get(n);
+	}
+
+	const result = fibWithCache(n - 2) + fibWithCache(n - 1);
+	cache.set(n, result);
+	return result;
 }
 
 function dpFib(n) {
-	const fib = [0, 1];
+	const fib = [...[0, 1], ...Array.from({ length: n - 1 })];
+
 	for (let i = 2; i <= n; i++) {
-		fib.push(fib[i - 1] + fib[i - 2]);
+		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
 	return fib[n];
@@ -18,6 +35,17 @@ function countTrajectories(n, allowed) {
 			k[i] = k[i - 1] + k[i - 2] + k[i - 3];
 		}
 	}
+
+	return k[n];
 }
 
-module.exports = { fib, dpFib, tragNum: countTrajectories };
+function countMinCost(n, price) {
+	const c = [null, price[1], price[1] + price[2], ...Array.from({ length: n - 2 })];
+	for (let i = 3; i <= n; i++) {
+		c[i] = price[i] + Math.min(c[i - 1], c[i - 2]);
+	}
+
+	return c[n];
+}
+
+module.exports = { fib, fibWithCache, dpFib, tragNum: countTrajectories, countMinCost };
--- 10/dp.test.js
+++ 10/dp.test.js
@@ -5,7 +5,17 @@ test('Fib', () => {
 	expect(dp.fib(8)).toBe(21);
 });
 
+test('Fib with cache', () => {
+	expect(dp.fibWithCache(5)).toBe(5);
+	expect(dp.fibWithCache(8)).toBe(21);
+});
+
 test('dp Fib', () => {
 	expect(dp.dpFib(5)).toBe(5);
 	expect(dp.dpFib(8)).toBe(21);
+});
+
+test('Count min cost', () => {
+	expect(dp.countMinCost(5, [null, 0, 2, 1, 1, 1])).toBe(2);
+	expect(dp.countMinCost(7, [null, 0, 2, 1, 1, 1, 1, 2])).toBe(4);
 });
